6583be738e6ed6b9200bf655230c65e8c6052918
ArisPutraBuana <arispb99@gmail.com>
2020-04-12 23:27:54 +0700

    Tutorial ke-4 (Directive v-if & v-else)

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -1,6 +1,32 @@
 var app = new Vue({
+  //   el: "#app",
+  //   data: {
+  //     show: true,
+  //     message: "Hello Aris Putra Buana",
+  //   },
+
+  //   el: "#app",
+  //   data: {
+  //     show: false,
+  //     message: "Hello Aris Putra Buana",
+  //   },
+
+  //   el: "#app",
+  //   data: {
+  //     show: false,
+  //     message: "Hello Aris Putra Buana",
+  //   },
+
+  //   el: "#app",
+  //   data: {
+  //     show: "ready",
+  //     message: "Hello Aris Putra Buana",
+  //   },
+
   el: "#app",
   data: {
-    message: "<i><b>Hello Aris Putra Buana</b></i>",
+    show: false,
+    message: "Hello Aris Putra Buana",
+    message2: "Hello Meyka Setyawati",
   },
 });
