eb9a10542d4d6316fac0787124ce7bc716913a0d
ArisPutraBuana <arispb99@gmail.com>
2020-04-14 01:19:43 +0700

    Tutorial ke-20 ( Attribut dgn nilai Dinamik )

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -1,13 +1,32 @@
-Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button @click="count++">Click Me!  {{ count }}  times</button>'
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div>
+      <h3>{{ post.title }}</h3>
+      <div v-html="post.content"></div>
+    </div>
+  `
 });
 
 var app = new Vue({
   el: "#app",
+  data: {
+    posts: [{
+        id: 1,
+        title: 'My journey with Vue',
+        content: 'This is Content of <strong>Post 1</strong>'
+      },
+      {
+        id: 2,
+        title: 'Blogging with Vue',
+        content: 'This is Content of <strong>Post 2</strong>'
+      },
+      {
+        id: 3,
+        title: 'Why Vue is so fun',
+        content: 'This is Content of <strong>Post 3</strong>'
+      }
+    ]
+  }
 
 });
