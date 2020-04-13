38cc237395fee5f18c656080efab646c174a20aa
ArisPutraBuana <arispb99@gmail.com>
2020-04-14 00:45:27 +0700

    Tutorial ke-19 ( Component HTML & Memanipulasi data )

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -1,11 +1,10 @@
-Vue.component('greeting', {
-  template: `
-    <div>
-      <b>Hello, Vue!</b></br>
-      <i>Hello, Vue!</i></br>
-      <u>Hello, Vue!</u>
-    </div>
-  `
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button @click="count++">Click Me!  {{ count }}  times</button>'
 });
 
 var app = new Vue({
