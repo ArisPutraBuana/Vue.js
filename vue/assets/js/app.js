4e136dee81b1b2dec3bf9848af459f8072c1ed22
ArisPutraBuana <arispb99@gmail.com>
2020-04-13 15:03:42 +0700

    Tutorial ke-6 {Method Properties (Menggunakan Method)}

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -1,22 +1,16 @@
 var app = new Vue({
-
   el: "#app",
   data: {
     int1: 5,
-    int2: 3
+    int2: 3,
+    result: null,
   },
-  computed: {
-    tambah: function () {
-      return this.int1 + this.int2;
+  methods: {
+    // sumProcess: function () {
+    // return (this.result = this.int2 + this.int1);
+
+    sumProcess: function (int3) {
+      return (this.result = this.int2 + this.int1 + int3);
     },
-    kali: function () {
-      return this.int1 * this.int2;
-    },
-    kurang: function () {
-      return this.int1 - this.int2;
-    },
-    bagi: function () {
-      return this.int1 / this.int2;
-    }
-  }
+  },
 });
