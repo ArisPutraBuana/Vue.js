aacd0b05fee0211e6f3588efe9eddb0a9b393df3
ArisPutraBuana <arispb99@gmail.com>
2020-04-13 11:06:32 +0700

    Tutorial ke-5 {Computed Properties (Kali,Bagi,Tambah,kurang)}

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -1,32 +1,22 @@
 var app = new Vue({
-  //   el: "#app",
-  //   data: {
-  //     show: true,
-  //     message: "Hello Aris Putra Buana",
-  //   },
-
-  //   el: "#app",
-  //   data: {
-  //     show: false,
-  //     message: "Hello Aris Putra Buana",
-  //   },
-
-  //   el: "#app",
-  //   data: {
-  //     show: false,
-  //     message: "Hello Aris Putra Buana",
-  //   },
-
-  //   el: "#app",
-  //   data: {
-  //     show: "ready",
-  //     message: "Hello Aris Putra Buana",
-  //   },
 
   el: "#app",
   data: {
-    show: false,
-    message: "Hello Aris Putra Buana",
-    message2: "Hello Meyka Setyawati",
+    int1: 5,
+    int2: 3
   },
+  computed: {
+    tambah: function () {
+      return this.int1 + this.int2;
+    },
+    kali: function () {
+      return this.int1 * this.int2;
+    },
+    kurang: function () {
+      return this.int1 - this.int2;
+    },
+    bagi: function () {
+      return this.int1 / this.int2;
+    }
+  }
 });
