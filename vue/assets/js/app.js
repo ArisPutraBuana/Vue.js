c0882170512ef81c7f91235dc2a56ffdd7fee6f9
ArisPutraBuana <arispb99@gmail.com>
2020-04-13 17:17:47 +0700

    Tutorial ke-8 {v-model in textbox (Membuat Form yg Realtime)}

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -1,17 +1,6 @@
 var app = new Vue({
   el: "#app",
   data: {
-    kilometers: 0,
-    meters: 0
+    message: ''
   },
-  watch: {
-    kilometers: function (val) {
-      this.kilometers = val;
-      this.meters = val * 1000;
-    },
-    meters: function (val) {
-      this.kilometers = val / 1000;
-      this.meters = val;
-    }
-  }
 });
