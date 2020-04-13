b73a9e92634e190359f36d26e831e1d0d9119dea
ArisPutraBuana <arispb99@gmail.com>
2020-04-14 00:32:34 +0700

    Tutorial ke-18 ( Component )

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -1,26 +1,14 @@
+Vue.component('greeting', {
+  template: `
+    <div>
+      <b>Hello, Vue!</b></br>
+      <i>Hello, Vue!</i></br>
+      <u>Hello, Vue!</u>
+    </div>
+  `
+});
+
 var app = new Vue({
   el: "#app",
-  data: {
-    vehicles: [{
-        type: 'Car',
-        brand: 'Toyota',
-        color: 'Metallic Silver'
-      },
-      {
-        type: 'Motocycle',
-        brand: 'Honda',
-        color: 'Red'
-      },
-      {
-        type: 'Bike',
-        brand: 'BMX',
-        color: 'Black'
-      }
-    ],
-    object: {
-      firstName: 'Aris',
-      lastName: 'Putra Buana',
-      age: 23
-    }
-  }
+
 });
