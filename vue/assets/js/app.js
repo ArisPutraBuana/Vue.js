f2ca49ebafc24bedd1bc703bc16bdac912759933
ArisPutraBuana <arispb99@gmail.com>
2020-04-13 22:50:21 +0700

    Tutorial ke-15 {v-on (Key Modifier)}

--- vue/assets/js/app.js
+++ vue/assets/js/app.js
@@ -4,8 +4,20 @@ var app = new Vue({
     message: ''
   },
   methods: {
-    showAlert: function () {
-      alert('The button has been clicked to call this method!');
-    }
+    escKey: function () {
+      this.message = 'You pressing esc key'
+    },
+    spaceKey: function () {
+      this.message = 'You pressing space key'
+    },
+    upKey: function () {
+      this.message = 'You pressing up key'
+    },
+    downKey: function () {
+      this.message = 'You pressing down key'
+    },
+    aKey: function () {
+      this.message = 'You pressing alphabet "a" key'
+    },
   }
 });
